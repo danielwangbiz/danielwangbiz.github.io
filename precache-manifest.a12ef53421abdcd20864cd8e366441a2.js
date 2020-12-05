bb4de4dc899176642623dc92a9321daffa2954f7
danielwangbiz <daniel.wang.biz@gmail.com>
2020-12-05 21:03:30 +0100

    demo

---
+++ precache-manifest.a12ef53421abdcd20864cd8e366441a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869a34cc7be7137d9d190c004f9f2282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c73b7d9fa4e7d2fdb78",
+    "url": "/static/css/main.0cdae5cc.chunk.css"
+  },
+  {
+    "revision": "1066ef9a8b2563112d76",
+    "url": "/static/js/2.b2b0ba11.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.b2b0ba11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c73b7d9fa4e7d2fdb78",
+    "url": "/static/js/main.8a4440e2.chunk.js"
+  },
+  {
+    "revision": "778813c0cf5e27038dc7",
+    "url": "/static/js/runtime-main.a0d4c309.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "0417539d1f6a7acb93136771535292ca",
+    "url": "/static/media/bitmap-copy.0417539d.png"
+  },
+  {
+    "revision": "d02324cac9b23b1e40e4ea8a4ecf7ab8",
+    "url": "/static/media/bitmap.d02324ca.png"
+  }
+]);
